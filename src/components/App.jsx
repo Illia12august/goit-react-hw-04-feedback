ebbd8c6d332655348e7e4ac4cfc81875154596a9
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-09-14 00:00:39 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,31 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import ButtonComponent from './ButtonComponent';
 import Statistics from './DataComponent';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
+  const reviews = {good, bad, neutral}
+  const onReviewChange = review => {
+    switch (review) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break
+      case 'bad':
+        setBad(prevState =>  prevState + 1);
+        break
+      default:
+        break
+    }
   };
-  onReviewChange = review => {
-    this.setState(prevState => {
-      return {
-        [review]: prevState[review] + 1,
-      };
-    });
-  };
-  render() {
-    return (
-      <>
-        <ButtonComponent onChange={this.onReviewChange}></ButtonComponent>
-        <Statistics reviews={this.state}></Statistics>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ButtonComponent onChange={onReviewChange}></ButtonComponent>
+      <Statistics reviews={reviews}></Statistics>
+    </>
+  );
+};
